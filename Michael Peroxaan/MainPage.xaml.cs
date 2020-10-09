e66dde443d6b1b88bbd527da6b3e0da67d1fdbc5
idkwuu <idkwhatusernameuse@gmail.com>
2020-10-09 12:38:09 -0600

    Initial commit

---
+++ Michael Peroxaan/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Michael_Peroxaan
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void showAlertDialog(object sender, RoutedEventArgs e)
+        {
+            await new Windows.UI.Popups.MessageDialog("Michael Peroxaan", "Michael Peroxaan").ShowAsync();
+        }
+    }
+}
